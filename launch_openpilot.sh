665c67f5788f8eb4068b6e9dc444bc708fc7eff3
rldmwl88 <69119025+rldmwl88@users.noreply.github.com>
2020-09-01 01:41:14 +0900

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -7,15 +7,15 @@ fi
 if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
     sleep 3
     mount -o remount,rw /system
-  	cp -rf /data/openpilot/selfdrive/assets/fonts/opensans* /system/fonts/
+    cp -rf /data/openpilot/selfdrive/assets/fonts/opensans* /system/fonts/
     cp -rf /data/openpilot/font/fonts.xml /system/etc/fonts.xml
     chmod 644 /system/etc/fonts.xml
-  	chmod 644 /system/fonts/opensans*
+    chmod 644 /system/fonts/opensans*
     mount -o remount,r /system
-	
-	setprop persist.sys.locale ko-KR
-  	setprop persist.sys.local ko-KR
-	setprop persist.sys.timezone Asia/Seoul
+    
+    setprop persist.sys.locale ko-KR
+    setprop persist.sys.local ko-KR
+    setprop persist.sys.timezone Asia/Seoul
 fi
 
 export PASSIVE="0"
